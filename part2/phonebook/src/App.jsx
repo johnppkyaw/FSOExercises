d33ebab28da39af7ddaf8721aeecd8d6cb13c3cd
John Kyaw <jjpyae@gmail.com>
2023-12-22 16:53:41 -0800

    2.12 complete

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -42,8 +42,13 @@ const App = () => {
         name: newName,
         number: newNumber
       };
-      setPersons(persons.concat(newPerson));
-      setNewName('');
+      axios
+        .post('http://localhost:3001/persons', newPerson)
+        .then(response => {
+          setPersons(persons.concat(response.data));
+          setNewName('');
+          setNewNumber('');
+        })
     }
   }
 
