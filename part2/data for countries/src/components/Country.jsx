64dd43ac8bf369b939af9b9de69e57abc2906ef9
John Kyaw <jjpyae@gmail.com>
2024-01-12 18:32:44 -0800

    2.18 complete

--- part2/data for countries/src/components/Country.jsx
+++ part2/data for countries/src/components/Country.jsx
@@ -8,8 +8,20 @@ const Country = ({filteredCountries}) => {
         )
     } else if (countryMatchCount <= 10) {
         if(countryMatchCount == 1) {
+            console.log(targetCountry);
             return (
+                <>
                 <h1>{targetCountry.name.common}</h1>
+                <div>capital {targetCountry.capital[0]}</div>
+                <div>area {targetCountry.area}</div>
+                <h3>languages:</h3>
+                <ul>
+                    {Object.values(targetCountry.languages).map(language => (
+                        <li>{language}</li>
+                    ))}
+                </ul>
+                <img src={targetCountry.flags.png} width="200" height="200" alt="the picture of the country flag"></img>
+                </>
             )
         } else {
             return (
@@ -18,7 +30,7 @@ const Country = ({filteredCountries}) => {
                 ))
             )
         }
-        
+
     }
 }
 
